ed6c55d1dbca045fda5a4bcd669dd12bc4c244e4
Chenglong Shi <86996944+2230307855@users.noreply.github.com>
2023-07-30 21:29:06 +0800

    配置文件数据库修改为提交远程的数据库-ChenglongShi
    配置文件数据库修改为提交远程的数据库-ChenglongShi

--- douyin/dao/database_init.go
+++ douyin/dao/database_init.go
@@ -24,17 +24,17 @@ func SetupDB() {
 	// )
 	//----------------------------------------------------------
 	//发布时候的配置
-	//username := "datatest"
-	//password := "EYxwxRsNfYTn7SN7"
-	//host := "111.92.243.152"
-	//port := 3306
-	//dbname := "datatest"
-	//本地运行配置------------------------------------------------
-	username := "root"
-	password := "123456"
-	host := "127.0.0.1"
+	username := "datatest"
+	password := "EYxwxRsNfYTn7SN7"
+	host := "111.92.243.152"
 	port := 3306
-	dbname := "gorm_test"
+	dbname := "datatest"
+	//本地运行配置------------------------------------------------
+	//username := "root"
+	//password := "123456"
+	//host := "127.0.0.1"
+	//port := 3306
+	//dbname := "gorm_test"
 
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local", username, password, host, port, dbname)
 
