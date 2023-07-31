d2c2f8cd8fe1c3711cc8a37dca6c9b15bfcf9e72
Chenglong Shi <86996944+2230307855@users.noreply.github.com>
2023-07-31 09:11:05 +0800

    修复AccessKey泄露-ChenglongShi
    修复AccessKey泄露-ChenglongShi

--- douyin/utils/ConfigParameters.go
+++ douyin/utils/ConfigParameters.go
@@ -2,7 +2,7 @@ package utils
 
 const EndPoint = "https://oss-cn-hongkong.aliyuncs.com"
 const AccessKeyId = "LTAI5t683ss76uq1hqbr3vdp"
-const AccessKeySecret = "N3Zkd0Z9Q44nqGj9UVfrAnUexW9m30"
+const AccessKeySecret = "输入您的AccessKey"
 const BurketName = "fadfaaerqrghgzfaetyqrafxfg"
 const OssFilePath = "douyin/videos/"
 const OssVisitPath = "https://img.ricemoon.cn/douyin/videos/"
