4036b6d2afe4ae717fade5701f500fec90a3b584
Chenglong Shi <86996944+2230307855@users.noreply.github.com>
2023-07-31 16:45:29 +0800

    修复信息泄露-ChenglongShi
    修复信息泄露-ChenglongShi

--- douyin/utils/ConfigParameters.go
+++ douyin/utils/ConfigParameters.go
@@ -1,15 +1,8 @@
 package utils
 
-//const EndPoint = "输入您的服务节点"
-//const AccessKeyId = "您的AccessKeyId"
-//const AccessKeySecret = "输入您的AccessKey"
-//const BurketName = "您的Burket名称"
-//const OssFilePath = "douyin/videos/"
-//const OssVisitPath = "https://img.ricemoon.cn/douyin/videos/"
-
-const EndPoint = "https://oss-cn-hangzhou.aliyuncs.com"
-const AccessKeyId = "LTAI5tR56Um75Bkpu9JCoc5t"
-const AccessKeySecret = "VmBihAwB1AvXJReFAZYQz2BVsAQCcX"
-const BurketName = "web-test-store"
+const EndPoint = "输入您的服务节点"
+const AccessKeyId = "您的AccessKeyId"
+const AccessKeySecret = "输入您的AccessKey"
+const BurketName = "您的Burket名称"
 const OssFilePath = "douyin/videos/"
-const OssVisitPath = "https://web-test-store.oss-cn-hangzhou.aliyuncs.com/douyin/videos/"
+const OssVisitPath = "https://img.ricemoon.cn/douyin/videos/"
